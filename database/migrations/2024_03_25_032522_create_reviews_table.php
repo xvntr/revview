e728eda55ff861849aa3075206a3dbfa9923b4c8
xvntr <164462454+xvntr@users.noreply.github.com>
2024-03-25 10:56:48 +0700

    setup relation database

--- database/migrations/2024_03_25_032522_create_reviews_table.php
+++ database/migrations/2024_03_25_032522_create_reviews_table.php
@@ -12,9 +12,13 @@ return new class extends Migration {
     {
         Schema::create('reviews', function (Blueprint $table) {
             $table->id();
+            // $table->unsignedBigInteger('book_id');
             $table->text('review');
             $table->unsignedTinyInteger('rating');
             $table->timestamps();
+            $table->foreignId('book_id')->constrained('')->cascadeOnDelete();
+            // $table->foreign('book_id')->references('id')->on('books')->onDelete('cascade');
+
         });
     }
 
