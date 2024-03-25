7bf0f54723ba77da2832d12b69d9b6d2589bcdf5
xvntr <164462454+xvntr@users.noreply.github.com>
2024-03-26 05:18:28 +0700

    update book shows and migrate to mysql

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -13,12 +13,20 @@ class BookController extends Controller
     public function index(Request $request)
     {
         $title = $request->input('title');
+        $filter = $request->input('filter', '');
 
         $books = Book::when(
             $title,
             fn($query, $title) => $query->title($title)
-        )
-            ->get();
+        );
+        $books = match ($filter) {
+            'popular_last_month' => $books->popularLastMonth(),
+            'popular_last_6months' => $books->popularLast6Months(),
+            'highest_rated_last_month' => $books->highestRatedLastMonth(),
+            'highest_rated_last_6months' => $books->highestRatedLast6Months(),
+            default => $books->latest()
+        };
+        $books = $books->get();
 
         return view('books.index', ['books' => $books]);
     }
@@ -42,9 +50,16 @@ class BookController extends Controller
     /**
      * Display the specified resource.
      */
-    public function show(string $id)
+    public function show(Book $book)
     {
-        //
+        return view(
+            'books.show',
+            [
+                'book' => $book->load([
+                    'reviews' => fn($query) => $query->latest()
+                ])
+            ]
+        );
     }
 
     /**
