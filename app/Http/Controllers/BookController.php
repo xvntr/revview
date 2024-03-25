253a559ee6517296021ac8df31ba905d15ec3f19
xvntr <164462454+xvntr@users.noreply.github.com>
2024-03-26 05:56:55 +0700

    beres add cache dan tambahan review

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -19,14 +19,23 @@ class BookController extends Controller
             $title,
             fn($query, $title) => $query->title($title)
         );
+
         $books = match ($filter) {
             'popular_last_month' => $books->popularLastMonth(),
             'popular_last_6months' => $books->popularLast6Months(),
             'highest_rated_last_month' => $books->highestRatedLastMonth(),
             'highest_rated_last_6months' => $books->highestRatedLast6Months(),
-            default => $books->latest()
+            default => $books->latest()->withAvgRating()->withReviewsCount()
         };
-        $books = $books->get();
+
+        $cacheKey = 'books:' . $filter . ':' . $title;
+        $books =
+            cache()->remember(
+                $cacheKey,
+                3600,
+                fn() =>
+                $books->get()
+            );
 
         return view('books.index', ['books' => $books]);
     }
@@ -50,16 +59,20 @@ class BookController extends Controller
     /**
      * Display the specified resource.
      */
-    public function show(Book $book)
+    public function show(int $id)
     {
-        return view(
-            'books.show',
-            [
-                'book' => $book->load([
-                    'reviews' => fn($query) => $query->latest()
-                ])
-            ]
+        $cacheKey = 'book:' . $id;
+
+        $book = cache()->remember(
+            $cacheKey,
+            3600,
+            fn() =>
+            Book::with([
+                'reviews' => fn($query) => $query->latest()
+            ])->withAvgRating()->withReviewsCount()->findOrFail($id)
         );
+
+        return view('books.show', ['book' => $book]);
     }
 
     /**
--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,17 @@
 
 use App\Http\Controllers\BookController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ReviewController;
+
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect()->route('books.index');
 });
 
-Route::resource('books', BookController::class);
+Route::resource('books', BookController::class)
+    ->only(['index', 'show']);
+
+Route::resource('books.reviews', ReviewController::class)
+    ->scoped(['review' => 'book'])
+    ->only(['create', 'store']);
+
